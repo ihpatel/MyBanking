921f4ee1f2ce6db0832f9b27700d2599df36624b
Ishan Patel <ihpatel@purdue.edu>
2023-07-25 13:14:35 -0500

    small edits

--- routes/api/users.js
+++ routes/api/users.js
@@ -68,27 +68,22 @@ router.post("/login", (req, res) => {
 
   // Find user by email
   User.findOne({ email }).then(user => {
-    // Check if user exists
     if (!user) {
       return res.status(404).json({ emailnotfound: "Email not found" });
     }
 
-    // Check password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        // User matched
-        // Create JWT Payload
         const payload = {
           id: user.id,
           name: user.name
         };
 
-        // Sign token
         jwt.sign(
           payload,
           keys.secretOrKey,
           {
-            expiresIn: 31556926 // 1 year in seconds
+            expiresIn: 31556926 
           },
           (err, token) => {
             res.json({
